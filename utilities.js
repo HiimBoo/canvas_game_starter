751af65d03498ff5e6f74692a031db9e3625b352
10n9 <thanhlong6899@gmail.com>
2020-11-15 18:50:55 +0700

    update 1

---
+++ utilities.js
@@ -0,0 +1,77 @@
+backgroundMusic.play();
+// Main
+function animate() {
+  ctx1.clearRect(0, 0, canvas.clientWidth, canvas.height);
+  ctx2.clearRect(0, 0, canvas.clientWidth, canvas.height);
+  ctx3.clearRect(0, 0, canvas.clientWidth, canvas.height);
+  ctx4.clearRect(0, 0, canvas.clientWidth, canvas.height);
+  ctx5.clearRect(0, 0, canvas.clientWidth, canvas.height);
+  handleRipples();
+  ctx2.drawImage(background_lvl2, 0, 0, canvas.width, canvas.height);
+  handleParticles();
+  frogger.draw();
+  frogger.update();
+
+  handleObstacles();
+  handleScoreBoard();
+  ctx4.drawImage(grass, 0, 0);
+  frame++;
+  requestAnimationFrame(animate);
+}
+animate();
+
+//event listeners
+window.addEventListener("keydown", function (e) {
+  keys = [];
+  keys[e.keyCode] = true;
+  if (keys[37] || keys[38] || keys[39] || keys[40]) {
+    frogger.jump();
+  }
+});
+
+window.addEventListener("keyup", function (e) {
+  delete keys[e.keyCode];
+  frogger.moving = false;
+  frogger.frameX = 0;
+});
+
+// Calculate Score
+function scored() {
+  jumpSound.play();
+  score++;
+  gameSpeed += 0.05;
+  frogger.x = canvas.width / 2 - frogger.width / 2;
+  frogger.y = canvas.height - frogger.height - 40;
+}
+
+// Score Board
+function handleScoreBoard() {
+  ctx4.fillStyle = "black";
+  ctx4.strokeStyle = "black";
+  ctx4.font = "15px Verdana";
+  ctx4.strokeText("Score", 265, 15);
+  ctx4.font = "60px Verdana";
+  ctx4.fillText(score, 270, 65);
+  ctx4.font = "15px Verdana";
+  ctx4.strokeText("Collisions: " + collisionsCount, 10, 175);
+  ctx4.strokeText("Game speed: " + gameSpeed.toFixed(1), 10, 195);
+}
+
+// Collisions Detection between two rectangles
+function collision(first, second) {
+  return !(
+    first.x > second.x + second.width ||
+    first.x + first.width < second.x ||
+    first.y > second.y + second.height ||
+    first.y + first.height < second.y
+  );
+}
+
+// Reset game
+function resetGame() {
+  frogger.x = canvas.width / 2 - frogger.width / 2;
+  frogger.y = canvas.height - frogger.height - 40;
+  score = 0;
+  collisionsCount++;
+  gameSpeed = 0.7;
+}
